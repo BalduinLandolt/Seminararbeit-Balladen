60f6c9398c39add179ebb9e2d7c16494a95a71c3
Balduin Landolt <balduin.landolt@hotmail.com>
2021-01-08 14:46:53 +0100

    Add initial script and data

---
+++ coding/extract.py
@@ -0,0 +1,112 @@
+import pandas as pd
+from Bio import AlignIO, Phylo
+from Bio.Phylo.TreeConstruction import *
+from Bio.Phylo.Consensus import *
+
+
+def read_data():
+    # read data from file
+    data = pd.read_excel('../Datenerhebung/spreadsheet.xlsx', sheet_name='Sheet2', index_col=0)
+
+    # remove empty rows and columns
+    cols = [0] + [c for c in data.columns if str(c).startswith('0.')]
+    data.drop(index=0, columns=cols, inplace=True)
+
+    # clean row names
+    data.index = [str(i).split(' - ')[0].replace(' ', '_') for i in data.index]
+
+    # Data should be good now
+    # -----------------------
+    # print(data)
+    return data
+
+
+def save_as_nexus(path, data):
+    # build nexus file
+    nex_01 = '''#NEXUS
+BEGIN data;
+Dimensions '''
+
+    nex_02 = ''';
+Format
+datatype=standard
+missing=?
+gap=- [Definiert den Datentyp und Symbole für fehlende Daten (?) und gaps (-)]
+Symbols="0123456789";
+Matrix
+'''
+
+    dimensions = f'ntax={data.shape[0]} nchar={data.shape[1]}'
+    prefix = nex_01 + dimensions + nex_02
+    matrix = get_matrix(data)
+    suffix = '\n;\nEND;\n'
+
+    nexus = prefix + matrix + suffix
+
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(nexus)
+
+
+def get_matrix(df):
+    rows = []
+    for i, row in enumerate(df.values):
+        name = df.index[i] + '____________'
+        name = name[:12]
+        rows.append(name + ' ' + ''.join(str(c) for c in row))
+    return '\n'.join(rows)
+
+
+def make_tree(path):
+    # Read nexus file
+    alignment = AlignIO.read(path, "nexus")
+    # print(alignment)
+
+    # calculate distance matrix
+    calculator = DistanceCalculator('identity')
+    dm = calculator.get_distance(alignment)
+    # print(dm)
+
+    # construct trees from distance matrix
+    constructor = DistanceTreeConstructor()
+    upgmatree = constructor.upgma(dm)
+    nj = constructor.nj(dm)
+    Phylo.draw_ascii(upgmatree)
+    Phylo.draw_ascii(nj)
+    # Phylo.draw(upgmatree)
+    # Phylo.draw(nj)
+    # nj.ladderize()
+    # phx = upgmatree.as_phyloxml()
+    # print(f'\n\nXML:\n\n{phx}')
+    # Phylo.draw_ascii(phx)
+    # phx.ladderize()
+    # Phylo.draw(phx)
+    # Phylo.draw_graphviz(phx)
+
+    # scorer = ParsimonyScorer()
+    # searcher = NNITreeSearcher(scorer)
+    # constructor = ParsimonyTreeConstructor(searcher)
+    # pars_tree = constructor.build_tree(alignment)  # FIXME: Why does that not work?
+    # Phylo.draw_ascii(pars_tree)
+
+    constructor = DistanceTreeConstructor(calculator)
+    # trees = bootstrap_trees(alignment, 100, constructor)
+    # print(list(trees))
+    consensus_tree = bootstrap_consensus(alignment, 100, constructor, majority_consensus)
+    # print(consensus_tree)
+    Phylo.draw_ascii(consensus_tree)
+    # print(consensus_tree)
+    # import sys
+    # Phylo.write(consensus_tree, sys.stdout, 'newick')
+    # Phylo.draw(consensus_tree)
+
+
+
+def main():
+    data = read_data()
+    nex = 'nexus.nex'
+    save_as_nexus(nex, data)
+    make_tree(nex)
+
+
+if __name__ == "__main__":
+    main()
